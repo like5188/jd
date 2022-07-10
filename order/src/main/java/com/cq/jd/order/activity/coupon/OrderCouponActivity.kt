76a4d7809304ef79e2400f96c25b51a332d8e9d7
like5188 <260864866@qq.com>
2022-07-10 16:32:00 +0800

    bug 215 优惠券,顶栏文本不一致;缺少下拉表

--- order/src/main/java/com/cq/jd/order/activity/coupon/OrderCouponActivity.kt
+++ order/src/main/java/com/cq/jd/order/activity/coupon/OrderCouponActivity.kt
@@ -2,7 +2,8 @@ package com.cq.jd.order.activity.coupon
 
 import android.os.Bundle
 import android.util.Log
-import androidx.core.content.ContextCompat
+import androidx.databinding.ObservableBoolean
+import androidx.databinding.ObservableInt
 import androidx.fragment.app.Fragment
 import com.common.library.ui.activity.BaseVmActivity
 import com.cq.jd.order.R
@@ -12,24 +13,39 @@ import com.cq.jd.order.databinding.OrderActivityCouponBinding
 import com.cq.jd.order.util.FragmentTabAdapter
 
 class OrderCouponActivity :
-    BaseVmActivity<OrderCouponModel, OrderActivityCouponBinding>(R.layout.order_activity_coupon),OrderCouponModelListener {
-
+    BaseVmActivity<OrderCouponModel, OrderActivityCouponBinding>(R.layout.order_activity_coupon), OrderCouponModelListener {
     private var tabAdapter: FragmentTabAdapter? = null
     private val fragments = ArrayList<Fragment>()
+    private val showSelectUi = ObservableBoolean(false)
+    private val selectedIndex = ObservableInt(1)
 
     override fun statusBarDarkFont(): Boolean = true
 
     override fun initWidget(savedInstanceState: Bundle?) {
         mDataBinding.model = mViewModel
+        mDataBinding.showSelectUi = showSelectUi
+        mDataBinding.selectedIndex = selectedIndex
+        mDataBinding.llTitle.setOnClickListener {
+            showSelectUi.set(!showSelectUi.get())
+        }
+        mDataBinding.clSelect.setOnClickListener {
+            showSelectUi.set(false)
+        }
+        mDataBinding.tvSelect0.setOnClickListener {
+            selectedIndex.set(0)
+            showSelectUi.set(false)
+        }
+        mDataBinding.tvSelect1.setOnClickListener {
+            selectedIndex.set(1)
+            showSelectUi.set(false)
+        }
+
         mViewModel.serOrderCouponModelListener(this)
 
         fragments.add(OrderCouponWaitFragment())
         fragments.add(OrderCouponLeadFragment())
 
         tabAdapter = FragmentTabAdapter(this, fragments, R.id.flChooseCoupon)
-
-        setTitleText("优惠券")
-        getTitleBar()?.setBackgroundColor(ContextCompat.getColor(this,R.color.white))
 
         tabAdapter?.setCurrentFragment(0)
     }
@@ -43,12 +59,12 @@ class OrderCouponActivity :
     }
 
     override fun clickWaitUse() {
-        Log.e("shit", "clickWaitUse: " )
+        Log.e("shit", "clickWaitUse: ")
         tabAdapter?.setCurrentFragment(0)
     }
 
     override fun clickLeadCoupon() {
-        Log.e("shit", "clickLeadCoupon: " )
+        Log.e("shit", "clickLeadCoupon: ")
         tabAdapter?.setCurrentFragment(1)
     }
 
